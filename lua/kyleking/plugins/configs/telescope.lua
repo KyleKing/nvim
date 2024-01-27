b6e1b28103813811e9348b2e9d8a4f4bb50a629c
Kyle King <KyleKing@users.noreply.github.com>
2024-01-27 05:06:19 -0500

    fix: load telescope options

--- lua/kyleking/plugins/configs/telescope.lua
+++ lua/kyleking/plugins/configs/telescope.lua
@@ -1,5 +1,33 @@
 local telescope = require "telescope"
+local actions = require "telescope.actions"
+-- PLANNED: local get_icon = require("astroui").get_icon
+local opts = {
+  defaults = {
+    -- PLANNED: git_worktrees = require("astrocore").config.git_worktrees,
+    -- prompt_prefix = get_icon("Selected", 1),
+    -- selection_caret = get_icon("Selected", 1),
+    path_display = { "truncate" },
+    sorting_strategy = "ascending",
+    layout_config = {
+      horizontal = { prompt_position = "top", preview_width = 0.55 },
+      vertical = { mirror = false },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+      n = { q = actions.close },
+    },
+  },
+}
 telescope.setup(opts)
+
 telescope.load_extension "fzf"
 
 -- [[ Configure Telescope ]]
--- lua/kyleking/plugins/telescope.lua
+++ lua/kyleking/plugins/telescope.lua
@@ -113,34 +113,5 @@ return {
     -- },
   },
   cmd = "Telescope",
-  opts = function()
-    local actions = require "telescope.actions"
-    -- PLANNED: local get_icon = require("astroui").get_icon
-    return {
-      defaults = {
-        -- PLANNED: git_worktrees = require("astrocore").config.git_worktrees,
-        -- prompt_prefix = get_icon("Selected", 1),
-        -- selection_caret = get_icon("Selected", 1),
-        path_display = { "truncate" },
-        sorting_strategy = "ascending",
-        layout_config = {
-          horizontal = { prompt_position = "top", preview_width = 0.55 },
-          vertical = { mirror = false },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
-        },
-        mappings = {
-          i = {
-            ["<C-n>"] = actions.cycle_history_next,
-            ["<C-p>"] = actions.cycle_history_prev,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-          },
-          n = { q = actions.close },
-        },
-      },
-    }
-  end,
   config = function(...) require "kyleking.plugins.configs.telescope" end,
 }
