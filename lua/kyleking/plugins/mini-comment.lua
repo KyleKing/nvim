2eb1609c81f1389f436dedb04e3be3a991252a06
Kyle King <KyleKing@users.noreply.github.com>
2024-01-27 12:00:58 -0500

    fix: update mini.comment configuration

--- lua/kyleking/plugins/mini-comment.lua
+++ lua/kyleking/plugins/mini-comment.lua
@@ -1,11 +1,14 @@
 -- Use gc/gb for toggling comments
 return {
   "echasnovski/mini.comment",
+  event = "BufReadPost",
   dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
-  -- FIXME: https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#minicomment
   opts = {
     hooks = {
       pre = function() require("ts_context_commentstring.internal").update_commentstring {} end,
     },
+    custom_commentstring = function()
+      return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+    end,
   },
 }
