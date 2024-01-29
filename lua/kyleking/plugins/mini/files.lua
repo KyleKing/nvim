727c68c5b6b9b9f137ae492ffcc245a8c4a345e1
Kyle King <KyleKing@users.noreply.github.com>
2024-01-29 00:21:09 -0500

    Revert "wip: try to use gitignore when sorting entries (however error that entries is nil)"
    This reverts commit 31aedb8e1981f0fb8db4dce42768f7cae5572fcf.

--- lua/kyleking/plugins/mini/files.lua
+++ lua/kyleking/plugins/mini/files.lua
@@ -29,30 +29,6 @@ return {
           and entry.name ~= ".venv"
           and entry.name ~= "node_modules"
       end,
-      sort = function(entries)
-        -- if entries then return entries end
-
-        -- technically can filter entries here too, and checking gitignore for _every entry individually_
-        -- like I would have to in `content.filter` above is too slow. Here we can give it _all_ the entries
-        -- at once, which is much more performant.
-        local all_paths = table.concat(vim.iter(entries):map(function(entry) return entry.path end):totable(), "\n")
-        local output_lines = {}
-        local job_id = vim.fn.jobstart({ "git", "check-ignore", "--stdin" }, {
-          stdout_buffered = true,
-          on_stdout = function(_, data) output_lines = data end,
-        })
-
-        -- command failed to run
-        if job_id < 1 then return entries end
-
-        -- send paths via STDIN
-        vim.fn.chansend(job_id, all_paths)
-        vim.fn.chanclose(job_id, "stdin")
-        vim.fn.jobwait { job_id }
-        return require("mini.files").default_sort(
-          vim.iter(entries):filter(function(entry) return not vim.tbl_contains(output_lines, entry.path) end):totable()
-        )
-      end,
     },
     windows = {
       -- Whether to show preview of file/directory under cursor
