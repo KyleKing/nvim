c1c17175bf77ffa2b689ad7f91b13fe4abb7247c
Kyle King <KyleKing@users.noreply.github.com>
2024-01-28 22:51:48 -0500

    feat: active ' w' and ' q' among other new keybinds from Astro

--- lua/kyleking/keybinds.lua
+++ lua/kyleking/keybinds.lua
@@ -38,7 +38,7 @@ vim.g.maplocalleader = "," -- set default local leader key
 --   return "dd"
 -- end, { expr = true })
 
--- -- Keymaps for better default experience
+-- -- KeyK("for better default experience
 -- -- See `:help vim.keymap.set()`
 -- vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
@@ -63,102 +63,119 @@ vim.g.maplocalleader = "," -- set default local leader key
 
 -- TODO: Finish importing astro core keymaps
 
--- -- Normal --
--- -- Standard Operations
--- maps.n["j"] = { "v:count == 0 ? 'gj' : 'j'", expr = true, desc = "Move cursor down" }
--- maps.n["k"] = { "v:count == 0 ? 'gk' : 'k'", expr = true, desc = "Move cursor up" }
--- maps.n["<Leader>w"] = { "<Cmd>w<CR>", desc = "Save" }
--- FIXME: maps.n["<Leader>w"] = { "<Cmd>w<CR>", desc = "Save" }
--- maps.n["<Leader>q"] = { "<Cmd>confirm q<CR>", desc = "Quit" }
--- FIXME: maps.n["<Leader>q"] = { "<Cmd>confirm q<CR>", desc = "Quit" }
--- maps.n["<Leader>Q"] = { "<Cmd>confirm qall<CR>", desc = "Quit all" }
--- maps.n["<Leader>n"] = { "<Cmd>enew<CR>", desc = "New File" }
--- maps.n["<C-s>"] = { "<Cmd>w!<CR>", desc = "Force write" }
--- maps.n["<C-q>"] = { "<Cmd>q!<CR>", desc = "Force quit" }
--- maps.n["|"] = { "<Cmd>vsplit<CR>", desc = "Vertical Split" }
--- maps.n["\\"] = { "<Cmd>split<CR>", desc = "Horizontal Split" }
--- maps.n["gx"] = { astro.system_open, desc = "Open the file under cursor with system app" }
+-- Normal --
+-- Standard Operations
+local K = vim.keymap.set
+-- K("n", "j", "v:count == 0 ? 'gj' : 'j'", expr = true, desc = "Move cursor down" })
+-- K("n", "k", "v:count == 0 ? 'gk' : 'k'", expr = true, desc = "Move cursor up" })
+K("n", "<Leader>w", "<Cmd>w<CR>", { desc = "Save" })
+K("n", "<Leader>q", "<Cmd>confirm q<CR>", { desc = "Quit" })
+K("n", "<Leader>Q", "<Cmd>confirm qall<CR>", { desc = "Quit all" })
+K("n", "<Leader>n", "<Cmd>enew<CR>", { desc = "New File" })
+K("n", "<C-s>", "<Cmd>w!<CR>", { desc = "Force write" })
+K("n", "<C-q>", "<Cmd>q!<CR>", { desc = "Force quit" })
+K("n", "|", "<Cmd>vsplit<CR>", { desc = "Vertical Split" })
+K("n", "\\", "<Cmd>split<CR>", { desc = "Horizontal Split" })
+-- K("n", "gx", astro.system_open, desc = "Open the file under cursor with system app" })
 
 -- -- Plugin Manager
--- maps.n["<Leader>pa"] = { function() require("astrocore").update_packages() end, desc = "Update Lazy and Mason" }
+-- K("n", "<Leader>pa", function() require("astrocore").update_packages() end, { desc = "Update Lazy and Mason" })
 
 -- -- Manage Buffers
--- maps.n["<Leader>c"] = { function() require("astrocore.buffer").close() end, desc = "Close buffer" }
--- maps.n["<Leader>C"] = { function() require("astrocore.buffer").close(0, true) end, desc = "Force close buffer" }
--- maps.n["]b"] = {
+-- K("n", "<Leader>c", function() require("astrocore.buffer").close() end, { desc = "Close buffer" })
+-- K("n", "<Leader>C", function() require("astrocore.buffer").close(0, true) end, { desc = "Force close buffer" })
+-- K(
+--   "n",
+--   "]b",
 --   function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
---   desc = "Next buffer",
--- }
--- maps.n["[b"] = {
+--   { desc = "Next buffer" }
+-- )
+-- K(
+--   "n",
+--   "[b",
 --   function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
---   desc = "Previous buffer",
--- }
--- maps.n[">b"] = {
+--   { desc = "Previous buffer" }
+-- )
+-- K(
+--   "n",
+--   ">b",
 --   function() require("astrocore.buffer").move(vim.v.count > 0 and vim.v.count or 1) end,
---   desc = "Move buffer tab right",
--- }
--- maps.n["<b"] = {
+--   { desc = "Move buffer tab right" }
+-- )
+-- K(
+--   "n",
+--   "<b",
 --   function() require("astrocore.buffer").move(-(vim.v.count > 0 and vim.v.count or 1)) end,
---   desc = "Move buffer tab left",
--- }
+--   { desc = "Move buffer tab left" }
+-- )
 
--- maps.n["<Leader>bc"] =
---   { function() require("astrocore.buffer").close_all(true) end, desc = "Close all buffers except current" }
--- maps.n["<Leader>bC"] = { function() require("astrocore.buffer").close_all() end, desc = "Close all buffers" }
--- maps.n["<Leader>bl"] =
---   { function() require("astrocore.buffer").close_left() end, desc = "Close all buffers to the left" }
--- maps.n["<Leader>bp"] = { function() require("astrocore.buffer").prev() end, desc = "Previous buffer" }
--- maps.n["<Leader>br"] =
---   { function() require("astrocore.buffer").close_right() end, desc = "Close all buffers to the right" }
--- maps.n["<Leader>bse"] = { function() require("astrocore.buffer").sort "extension" end, desc = "By extension" }
--- maps.n["<Leader>bsr"] = { function() require("astrocore.buffer").sort "unique_path" end, desc = "By relative path" }
--- maps.n["<Leader>bsp"] = { function() require("astrocore.buffer").sort "full_path" end, desc = "By full path" }
--- maps.n["<Leader>bsi"] = { function() require("astrocore.buffer").sort "bufnr" end, desc = "By buffer number" }
--- maps.n["<Leader>bsm"] = { function() require("astrocore.buffer").sort "modified" end, desc = "By modification" }
+-- K(
+--   "n",
+--   "<Leader>bc",
+--   function() require("astrocore.buffer").close_all(true) end,
+--   { desc = "Close all buffers except current" }
+-- )
+-- K("n", "<Leader>bC", function() require("astrocore.buffer").close_all() end, { desc = "Close all buffers" })
+-- K(
+--   "n",
+--   "<Leader>bl",
+--   function() require("astrocore.buffer").close_left() end,
+--   { desc = "Close all buffers to the left" }
+-- )
+-- K("n", "<Leader>bp", function() require("astrocore.buffer").prev() end, { desc = "Previous buffer" })
+-- K(
+--   "n",
+--   "<Leader>br",
+--   function() require("astrocore.buffer").close_right() end,
+--   { desc = "Close all buffers to the right" }
+-- )
+-- K("n", "<Leader>bse", function() require("astrocore.buffer").sort "extension" end, { desc = "By extension" })
+-- K("n", "<Leader>bsr", function() require("astrocore.buffer").sort "unique_path" end, { desc = "By relative path" })
+-- K("n", "<Leader>bsp", function() require("astrocore.buffer").sort "full_path" end, { desc = "By full path" })
+-- K("n", "<Leader>bsi", function() require("astrocore.buffer").sort "bufnr" end, { desc = "By buffer number" })
+-- K("n", "<Leader>bsm", function() require("astrocore.buffer").sort "modified" end, { desc = "By modification" })
 
--- maps.n["<Leader>ld"] = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" }
--- maps.n["[d"] = { function() vim.diagnostic.goto_prev() end, desc = "Previous diagnostic" }
--- maps.n["]d"] = { function() vim.diagnostic.goto_next() end, desc = "Next diagnostic" }
--- maps.n["gl"] = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" }
+-- K("n", "<Leader>ld", function() vim.diagnostic.open_float() end, { desc = "Hover diagnostics" })
+-- K("n", "[d", function() vim.diagnostic.goto_prev() end, { desc = "Previous diagnostic" })
+-- K("n", "]d", function() vim.diagnostic.goto_next() end, { desc = "Next diagnostic" })
+-- K("n", "gl", function() vim.diagnostic.open_float() end, { desc = "Hover diagnostics" })
 
 -- -- Navigate tabs
--- maps.n["]t"] = { function() vim.cmd.tabnext() end, desc = "Next tab" }
--- maps.n["[t"] = { function() vim.cmd.tabprevious() end, desc = "Previous tab" }
+-- K("n", "]t", function() vim.cmd.tabnext() end, { desc = "Next tab" })
+-- K("n", "[t", function() vim.cmd.tabprevious() end, { desc = "Previous tab" })
 
 -- -- Split navigation
--- maps.n["<C-h>"] = { "<C-w>h", desc = "Move to left split" }
--- maps.n["<C-j>"] = { "<C-w>j", desc = "Move to below split" }
--- maps.n["<C-k>"] = { "<C-w>k", desc = "Move to above split" }
--- maps.n["<C-l>"] = { "<C-w>l", desc = "Move to right split" }
--- maps.n["<C-Up>"] = { "<Cmd>resize -2<CR>", desc = "Resize split up" }
--- maps.n["<C-Down>"] = { "<Cmd>resize +2<CR>", desc = "Resize split down" }
--- maps.n["<C-Left>"] = { "<Cmd>vertical resize -2<CR>", desc = "Resize split left" }
--- maps.n["<C-Right>"] = { "<Cmd>vertical resize +2<CR>", desc = "Resize split right" }
+-- K("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+-- K("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
+-- K("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
+-- K("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+-- K("n", "<C-Up>", "<Cmd>resize -2<CR>", { desc = "Resize split up" })
+-- K("n", "<C-Down>", "<Cmd>resize +2<CR>", { desc = "Resize split down" })
+-- K("n", "<C-Left>", "<Cmd>vertical resize -2<CR>", { desc = "Resize split left" })
+-- K("n", "<C-Right>", "<Cmd>vertical resize +2<CR>", { desc = "Resize split right" })
 
 -- -- Stay in indent mode
--- maps.v["<S-Tab>"] = { "<gv", desc = "Unindent line" }
--- maps.v["<Tab>"] = { ">gv", desc = "Indent line" }
+-- K("v", "<S-Tab>", "<gv", { desc = "Unindent line" })
+-- K("v", "<Tab>", ">gv", { desc = "Indent line" })
 
 -- -- Improved Terminal Navigation
--- maps.t["<C-h>"] = { "<Cmd>wincmd h<CR>", desc = "Terminal left window navigation" }
--- maps.t["<C-j>"] = { "<Cmd>wincmd j<CR>", desc = "Terminal down window navigation" }
--- maps.t["<C-k>"] = { "<Cmd>wincmd k<CR>", desc = "Terminal up window navigation" }
--- maps.t["<C-l>"] = { "<Cmd>wincmd l<CR>", desc = "Terminal right window navigation" }
+-- K("t", "<C-h>", "<Cmd>wincmd h<CR>", { desc = "Terminal left window navigation" })
+-- K("t", "<C-j>", "<Cmd>wincmd j<CR>", { desc = "Terminal down window navigation" })
+-- K("t", "<C-k>", "<Cmd>wincmd k<CR>", { desc = "Terminal up window navigation" })
+-- K("t", "<C-l>", "<Cmd>wincmd l<CR>", { desc = "Terminal right window navigation" })
 
--- -- -- Custom menu for modification of the user experience
--- -- maps.n["<Leader>ub"] = { function() require("astrocore.toggles").background() end, desc = "Toggle background" }
--- -- maps.n["<Leader>ug"] = { function() require("astrocore.toggles").signcolumn() end, desc = "Toggle signcolumn" }
--- -- maps.n["<Leader>uh"] = { function() require("astrocore.toggles").foldcolumn() end, desc = "Toggle foldcolumn" }
--- -- maps.n["<Leader>ui"] = { function() require("astrocore.toggles").indent() end, desc = "Change indent setting" }
--- -- maps.n["<Leader>ul"] = { function() require("astrocore.toggles").statusline() end, desc = "Toggle statusline" }
--- -- maps.n["<Leader>un"] = { function() require("astrocore.toggles").number() end, desc = "Change line numbering" }
--- -- maps.n["<Leader>uN"] =
--- --   { function() require("astrocore.toggles").notifications() end, desc = "Toggle Notifications" }
--- -- maps.n["<Leader>up"] = { function() require("astrocore.toggles").paste() end, desc = "Toggle paste mode" }
--- -- maps.n["<Leader>us"] = { function() require("astrocore.toggles").spell() end, desc = "Toggle spellcheck" }
--- -- maps.n["<Leader>uS"] = { function() require("astrocore.toggles").conceal() end, desc = "Toggle conceal" }
--- -- maps.n["<Leader>ut"] = { function() require("astrocore.toggles").tabline() end, desc = "Toggle tabline" }
--- -- maps.n["<Leader>uu"] = { function() require("astrocore.toggles").url_match() end, desc = "Toggle URL highlight" }
--- -- maps.n["<Leader>uw"] = { function() require("astrocore.toggles").wrap() end, desc = "Toggle wrap" }
--- -- maps.n["<Leader>uy"] =
--- --   { function() require("astrocore.toggles").buffer_syntax() end, desc = "Toggle syntax highlight" }
+-- -- Custom menu for modification of the user experience
+-- K("n", "<Leader>ub", function() require("astrocore.toggles").background() end, { desc = "Toggle background" })
+-- K("n", "<Leader>ug", function() require("astrocore.toggles").signcolumn() end, { desc = "Toggle signcolumn" })
+-- K("n", "<Leader>uh", function() require("astrocore.toggles").foldcolumn() end, { desc = "Toggle foldcolumn" })
+-- K("n", "<Leader>ui", function() require("astrocore.toggles").indent() end, { desc = "Change indent setting" })
+-- K("n", "<Leader>ul", function() require("astrocore.toggles").statusline() end, { desc = "Toggle statusline" })
+-- K("n", "<Leader>un", function() require("astrocore.toggles").number() end, { desc = "Change line numbering" })
+-- K("n", "<Leader>uN", function() require("astrocore.toggles").notifications() end, { desc = "Toggle Notifications" })
+-- K("n", "<Leader>up", function() require("astrocore.toggles").paste() end, { desc = "Toggle paste mode" })
+-- K("n", "<Leader>us", function() require("astrocore.toggles").spell() end, { desc = "Toggle spellcheck" })
+-- K("n", "<Leader>uS", function() require("astrocore.toggles").conceal() end, { desc = "Toggle conceal" })
+-- K("n", "<Leader>ut", function() require("astrocore.toggles").tabline() end, { desc = "Toggle tabline" })
+-- K("n", "<Leader>uu", function() require("astrocore.toggles").url_match() end, { desc = "Toggle URL highlight" })
+-- K("n", "<Leader>uw", function() require("astrocore.toggles").wrap() end, { desc = "Toggle wrap" })
+-- K("n", "<Leader>uy", function() require("astrocore.toggles").buffer_syntax() end, { desc = "Toggle syntax highlight" })
