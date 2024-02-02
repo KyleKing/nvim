c151e63814101c09fd594f8446f759a3a64f1d24
Kyle King <KyleKing@users.noreply.github.com>
2024-02-02 04:41:13 -0500

    feat: migrate from tabout to neotab

---
+++ lua/kyleking/plugins/motion/neotab.lua
@@ -0,0 +1,12 @@
+return {
+  "kawre/neotab.nvim",
+  event = "InsertEnter",
+  opts = {
+    -- FYI: getting tab to work requires extra configuration
+    tabkey = "<C-f>",
+    -- act_as_tab = false, -- Having this produce a tab might be a useful side-effect
+    smart_punctuators = {
+      enabled = false,
+    },
+  },
+}
