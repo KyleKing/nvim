ae6eb7f03dde86e1e357296ac99646b1f3771054
Kyle King <KyleKing@users.noreply.github.com>
2024-02-02 10:29:30 -0500

    refactor: is the highlight on yank autocmd working?

--- lua/kyleking/autocmds.lua
+++ lua/kyleking/autocmds.lua
@@ -1,6 +1,6 @@
 -- Adapted from: https://github.com/sQVe/dotfiles/blob/b59afd70e10daae49f21bd5f7279858463a711e3/config/nvim/lua/sQVe/config/autocmds.lua
 local autocmd = vim.api.nvim_create_autocmd
-local augroup = function(name) return vim.api.nvim_create_augroup(name, { clear = true }) end
+local function augroup(name) return vim.api.nvim_create_augroup(name, { clear = true }) end
 
 local augroups = {}
 local augroup_keys = {
@@ -12,11 +12,12 @@ for _, augroup_key in ipairs(augroup_keys) do
    table.insert(augroups, augroup(augroup_key))
 end
 
--- Highlight yanked text.
-autocmd("TextYankPost", {
-   group = augroups.HighlightYank,
-   callback = function() vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 }) end,
-})
+-- PLANNED: I don't this this command was working
+-- -- Highlight yanked text.
+-- autocmd("TextYankPost", {
+--    group = augroups.HighlightYank,
+--    callback = function() vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 }) end,
+-- })
 
 -- Reload buffer on enter or focus.
 autocmd({ "BufEnter", "FocusGained" }, {
