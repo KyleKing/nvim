a8a8387493443849338e487fa5147a3d2453afe8
Kyle King <KyleKing@users.noreply.github.com>
2024-02-03 03:04:53 -0500

    refactor: remove lazy/auto-session workaround

--- lua/kyleking/plugins/session/auto-session.lua
+++ lua/kyleking/plugins/session/auto-session.lua
@@ -1,50 +1,6 @@
-local function handle_restore_when_lazy_syncs()
-   -- From: https://github.com/rmagatti/auto-session/issues/223#issuecomment-1666658887
-   local autocmd = vim.api.nvim_create_autocmd
-
-   local lazy_did_show_install_view = false
-
-   local function auto_session_restore()
-      -- important! without vim.schedule other necessary plugins might not load (eg treesitter) after restoring the session
-      vim.schedule(function() require("auto-session").AutoRestoreSession() end)
-   end
-
-   autocmd("User", {
-      pattern = "VeryLazy",
-      callback = function()
-         local lazy_view = require("lazy.view")
-
-         if lazy_view.visible() then
-            -- if lazy view is visible do nothing with auto-session
-            lazy_did_show_install_view = true
-         else
-            -- otherwise load (by require'ing) and restore session
-            auto_session_restore()
-         end
-      end,
-   })
-
-   autocmd("WinClosed", {
-      pattern = "*",
-      callback = function(ev)
-         local lazy_view = require("lazy.view")
-
-         -- if lazy view is currently visible and was shown at startup
-         if lazy_view.visible() and lazy_did_show_install_view then
-            -- if the window to be closed is actually the lazy view window
-            if ev.match == tostring(lazy_view.view.win) then
-               lazy_did_show_install_view = false
-               auto_session_restore()
-            end
-         end
-      end,
-   })
-end
-
 return {
    "rmagatti/auto-session",
    lazy = false,
-   -- lazy = true, -- FYI: resolve conflict with lazy.nvim panel on start (in combination with handle_restore_when_lazy_syncs)
    dependencies = { "nvim-telescope/telescope.nvim" }, -- Required for search_session
    opts = {
       auto_save_enabled = true,
@@ -57,9 +13,6 @@ return {
    init = function()
       -- Recommend in docs for best experience
       vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-
-      -- PLANNED: is this still necessary now that I'm only using buffers and not tabs?
-      -- handle_restore_when_lazy_syncs()
    end,
    keys = {
       { "<leader>Sd", "<cmd>SessionDelete<CR>", desc = "Delete Session" },
