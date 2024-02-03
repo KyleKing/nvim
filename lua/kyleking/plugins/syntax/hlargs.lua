b6073939584ee56129687f44b1d1a64323f5cbe6
Kyle King <KyleKing@users.noreply.github.com>
2024-02-03 14:24:18 -0500

    fix: trying to fix hlargs

--- lua/kyleking/plugins/syntax/hlargs.lua
+++ lua/kyleking/plugins/syntax/hlargs.lua
@@ -1,8 +1,21 @@
 return {
     "m-demare/hlargs.nvim",
-    event = "BufReadPost",
+    event = "BufRead",
     opts = {
         color = "#FF7A00", --"#ef9062",
-        paint_arg_usages = true,
+        paint_catch_blocks = {
+            declarations = true,
+            usages = true,
+        },
+        extras = {
+            named_parameters = true,
+        },
+    },
+    keys = {
+        {
+            "<leader>uA",
+            function() require("hlargs").toggle() end,
+            desc = "Toggle Highlight Args (hlargs)",
+        },
     },
 }
