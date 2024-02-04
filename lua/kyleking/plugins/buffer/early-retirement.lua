a17afed7d0931e3f29b187954c333922fa5da90a
Kyle King <KyleKing@users.noreply.github.com>
2024-02-04 11:58:32 -0500

    refactor: tweak early-retirement settings

--- lua/kyleking/plugins/buffer/early-retirement.lua
+++ lua/kyleking/plugins/buffer/early-retirement.lua
@@ -2,7 +2,12 @@ return {
     "chrisgrieser/nvim-early-retirement",
     event = "BufReadPost",
     opts = {
+        retirementAgeMins = 60,
+        minimumBufferNum = 20, -- Only when too many open buffers
+        notificationOnAutoClose = true, -- Show notification on closing using nvim-notify
+
         -- When a file is deleted, for example via an external program, delete the associated buffer as well
+        -- (This feature is independent from the automatic closing)
         deleteBufferWhenFileDeleted = true,
     },
 }
