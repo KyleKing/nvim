3e2adf1514d7dd237e868fcbcc2192887b5f6bd8
Kyle King <KyleKing@users.noreply.github.com>
2024-02-04 11:54:25 -0500

    refactor: improve toggle color autocmd

--- lua/kyleking/plugins/color/ccc.lua
+++ lua/kyleking/plugins/color/ccc.lua
@@ -8,7 +8,7 @@ return {
         { "<leader>zp", "<cmd>CccPick<cr>", desc = "Pick Color" },
     },
     opts = {
-        default_color = "#40bfbf",
+        default_color = "#40BFBF", -- FYI: just used for manually testing on changes
         highlighter = {
             auto_enable = true,
             lsp = true,
@@ -17,13 +17,17 @@ return {
     init = function()
         -- Toggle highlights when entering then leaving visual mode to avoid visual conflicts
         -- Source: https://github.com/uga-rosa/ccc.nvim/issues/78#issuecomment-1562682423
+        --  and: https://vi.stackexchange.com/a/38571/44707
+        -- FYI: pressing 'v' while in visual-line mode triggers higlight enable, but that's actually sort of a feature
         vim.api.nvim_create_autocmd("ModeChanged", {
-            pattern = "*:[vV\\x16]",
+            pattern = "*:[vV\\x16]*",
             callback = function() vim.cmd.CccHighlighterDisable() end,
+            desc = "Disable Color Highlight when entering visual mode",
         })
         vim.api.nvim_create_autocmd("ModeChanged", {
-            pattern = "[vV\\x16]:*",
+            pattern = "[vV\\x16]*:*",
             callback = function() vim.cmd.CccHighlighterEnable() end,
+            desc = "Enable Color Highlight when leaving visual mode",
         })
     end,
 }
