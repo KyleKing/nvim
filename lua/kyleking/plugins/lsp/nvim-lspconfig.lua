96a83b8e15ec5d3599ae7360eb2fc8512e041e34
Kyle King <KyleKing@users.noreply.github.com>
2024-02-07 06:26:21 -0500

    ci: run pre-commit

--- lua/kyleking/plugins/lsp/nvim-lspconfig.lua
+++ lua/kyleking/plugins/lsp/nvim-lspconfig.lua
@@ -129,10 +129,11 @@ return {
     init = function()
         -- Source: https://vi.stackexchange.com/a/39075/44707
         local border = "single"
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with( vim.lsp.handlers.hover, { border = border })
-        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with( vim.lsp.handlers.signature_help, { border = border })
-        vim.diagnostic.config{ float={border=border} }
-        require('lspconfig.ui.windows').default_options = { border = border}
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+        vim.lsp.handlers["textDocument/signatureHelp"] =
+            vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+        vim.diagnostic.config({ float = { border = border } })
+        require("lspconfig.ui.windows").default_options = { border = border }
     end,
     config = config,
 }
