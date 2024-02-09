e37415c51ea56e0195553f4fed69acd90874e2da
Kyle King <KyleKing@users.noreply.github.com>
2024-02-09 13:20:57 -0500

    feat: add placeholder mason and harpoon configs

---
+++ lua/kyleking/plugins/lsp/mason.lua
@@ -0,0 +1,43 @@
+return {
+    "williamboman/mason.nvim",
+    enabled = false, -- PLANNED: Copied from: https://github.com/JackDerksen/viis-lazyvim/blob/e3b59d9ff078894e06f5d0290f5f1aac2446437d/lua/plugins/mason.lua
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts = {
+        ensure_installed = {
+            "stylua",
+            "shfmt",
+        },
+        ui = {
+            border = "rounded",
+        },
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+        require("mason").setup(opts)
+        local mr = require("mason-registry")
+        mr:on("package:install:success", function()
+            vim.defer_fn(
+                function()
+                    require("lazy.core.handler.event").trigger({
+                        event = "FileType",
+                        buf = vim.api.nvim_get_current_buf(),
+                    })
+                end,
+                100
+            )
+        end)
+        local function ensure_installed()
+            for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then p:install() end
+            end
+        end
+        if mr.refresh then
+            mr.refresh(ensure_installed)
+        else
+            ensure_installed()
+        end
+    end,
+}
--- lua/kyleking/plugins/marks/harpoon.lua
+++ lua/kyleking/plugins/marks/harpoon.lua
@@ -4,25 +4,48 @@ return {
     enabled = false, -- PLANNED: investigate
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
-    init = function()
-        -- PLANNED: refactor to use standard Lazy opts/keys
-        local harpoon = require("harpoon")
-
-        -- REQUIRED
-        harpoon:setup()
-        -- REQUIRED
-
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
-        -- -- Toggle previous & next buffers stored within Harpoon list
-        -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-    end,
+    event = "UIEnter",
+    opts = {},
+    keys = {
+        {
+            "<leader>ha",
+            function() require('harpoon.mark').add_file() end,
+            desc = "Mark file with Harpoon",
+        },
+        {
+            "<leader>he",
+            function() require('harpoon.ui').toggle_quick_menu(require('harpoon'):list()) end,
+            desc = "Toggle Harpoon menu",
+        },
+        {
+            "<leader>h1",
+            function() require('harpoon.ui').nav_file(1) end,
+            desc = "Harpoon mark 1",
+        },
+        {
+            "<leader>h2",
+            function() require('harpoon.ui').nav_file(2) end,
+            desc = "Harpoon mark 2",
+        },
+        {
+            "<leader>h3",
+            function() require('harpoon.ui').nav_file(3) end,
+            desc = "Harpoon mark 3",
+        },
+        {
+            "<leader>h4",
+            function() require('harpoon.ui').nav_file(4) end,
+            desc = "Harpoon mark 4",
+        },
+        {
+            "<leader>hp",
+            function() require("harpoon"):list():prev() end,
+            desc = "Toggle Previous Buffer",
+        },
+        {
+            "<leader>hn",
+            function() require("harpoon"):list():next() end,
+            desc = "Toggle Next Buffer",
+        },
+    },
 }
