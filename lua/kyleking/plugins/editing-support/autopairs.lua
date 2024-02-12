6a3cab759d10075b7b5f757015a3bbe1eb6b92ce
Kyle King <KyleKing@users.noreply.github.com>
2024-02-11 20:10:52 -0500

    fix: add <C-l> mapping for autopairs

--- lua/kyleking/plugins/editing-support/autopairs.lua
+++ lua/kyleking/plugins/editing-support/autopairs.lua
@@ -2,7 +2,6 @@ return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     dependencies = {
-        { "nvim-treesitter/nvim-treesitter" },
         { "hrsh7th/nvim-cmp" },
     },
     config = function()
@@ -11,6 +10,12 @@ return {
         local cmp = require("cmp")
         -- If you want insert `(` after select function or method item
         cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+
+        -- Workaround to step over the added pairs
+        -- Alternatively, consider tabout or more complicated logic
+        -- https://github.com/windwp/nvim-autopairs/issues/383
+        -- https://github.com/windwp/nvim-autopairs/issues/167#issuecomment-1317652644
+        vim.keymap.set("i", "<C-l>", "<esc>:exe 'norm! l%%'<CR>a", { silent = true })
 
         -- FYI: Additional custom rules: https://github.com/windwp/nvim-autopairs/wiki/Custom-rules
     end,
