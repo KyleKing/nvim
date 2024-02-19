fc013dbbfb37a853811a8edadb01424ec4027215
Kyle King <KyleKing@users.noreply.github.com>
2024-02-19 09:18:14 -0500

    feat: turn on spell check

--- lua/kyleking/options.lua
+++ lua/kyleking/options.lua
@@ -7,10 +7,9 @@ vim.opt.breakindent = true -- wrap indent to match  line start
 vim.opt.clipboard = "unnamedplus" -- connection to the system clipboard
 vim.opt.cmdheight = 0 -- hide command line unless needed
 vim.opt.completeopt = { "menu", "menuone", "noselect" } -- Options for insert mode completion
-vim.opt.copyindent = true -- copy the previous indentation on autoindenting
+vim.opt.copyindent = true -- copy the previous indentation on auto-indenting
 vim.opt.cursorline = true -- highlight the text line of the cursor
 vim.opt.diffopt:append({ "algorithm:histogram", "linematch:60" }) -- enable linematch diff algorithm
-vim.opt.expandtab = true -- enable the use of space in tab
 vim.opt.fileencoding = "utf-8" -- file content encoding for the buffer
 vim.opt.fillchars = { eob = " " } -- disable `~` on nonexistent lines
 vim.opt.foldlevelstart = 99 -- start with all code unfolded
@@ -22,9 +21,8 @@ vim.opt.linebreak = true -- wrap lines at 'breakat'
 vim.opt.mouse = "a" -- enable mouse support
 vim.opt.number = true -- show numberline
 vim.opt.preserveindent = true -- preserve indent structure as much as possible
-vim.opt.pumheight = 10 -- height of the pop up menu
+vim.opt.pumheight = 15 -- height of the pop up menu
 vim.opt.relativenumber = true -- show relative numberline
-vim.opt.shiftwidth = 2 -- number of space inserted for indentation
 vim.opt.shortmess:append({ s = true, I = true }) -- disable search count wrap and startup messages
 vim.opt.showmode = false -- disable showing modes in command line
 vim.opt.showtabline = 1 -- (default) show tabline only if more than one tab
@@ -32,7 +30,6 @@ vim.opt.signcolumn = "yes" -- always show the sign column
 vim.opt.smartcase = true -- case sensitive searching when \C or a capital in search
 vim.opt.splitbelow = true -- splitting a new window below the current one
 vim.opt.splitright = true -- splitting a new window at the right of the current one
-vim.opt.tabstop = 2 -- number of space in a tab
 vim.opt.termguicolors = true -- enable 24-bit RGB color in the TUI
 vim.opt.timeoutlen = 500 -- shorten key timeout length a little bit for which-key
 vim.opt.title = true -- set terminal title to the filename and path
@@ -45,6 +42,13 @@ vim.opt.writebackup = false -- disable making a backup before overwriting a file
 -- vim.opt.colorcolumn = "80,120" -- highlighted screen columns (switched to smartcolumn plugin instead)
 vim.opt.scrolloff = 16 -- Number of screen lines to keep above and below the cursor
 vim.opt.wildmode = "full:lastused" -- complete the first full match immediately <https://stackoverflow.com/a/76471693/3219667>
+vim.opt.spelllang = 'en_us' -- Use the US dictionary (and any associated custom dictionaries)
+vim.opt.spell = true -- Always on spell checking
+
+-- Default indentation (unless overridden by editorconfig)
+vim.opt.expandtab = true -- enable the use of space in tab
+vim.opt.shiftwidth = 4 -- number of space inserted for indentation
+vim.opt.tabstop = 4 -- number of space in a tab
 
 -- Custom filetypes
 vim.filetype.add({
