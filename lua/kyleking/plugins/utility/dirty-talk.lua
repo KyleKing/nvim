d156344f77ef1be50f46171452a9d2a63efc4574
Kyle King <KyleKing@users.noreply.github.com>
2024-02-21 14:17:46 -0500

    feat: sort user spelllang

--- lua/kyleking/plugins/utility/dirty-talk.lua
+++ lua/kyleking/plugins/utility/dirty-talk.lua
@@ -6,8 +6,9 @@ return {
     config = function() vim.opt.spelllang = { "en_us", "programming" } end,
     keys = {
         {
+            -- From: https://github.com/nickjj/dotfiles/blob/d3c2b74f50e786edf78eceaa5359145f6f370eb3/.config/zsh/.aliases#L47C12-L47C86
             "<leader>pzs",
-            "<Cmd>!sort -u ${HOME}/.config/nvim/spell/en.utf-8.add -o ${HOME}/.config/nvim/spell/en.utf-8.add<CR>",
+            "<Cmd>!sort -u ${HOME}/.config/nvim/spell/en.utf-8.add --output=${HOME}/.config/nvim/spell/en.utf-8.add --unique --ignore-case<CR>",
             desc = "Shell Sort English Spelllang",
         },
     },
