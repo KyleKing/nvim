7ad07fd17ca7f592d198770385a0beb7c80cfa49
Kyle King <KyleKing@users.noreply.github.com>
2024-02-29 08:27:08 -0500

    feat: placeholder bin_discovery and nvim-lint updates

--- lua/kyleking/plugins/lsp/nvim-lint.lua
+++ lua/kyleking/plugins/lsp/nvim-lint.lua
@@ -1,35 +1,51 @@
 return {
     "mfussenegger/nvim-lint",
-    event = {
-        "BufReadPre",
-        "BufNewFile",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        { "stevearc/conform.nvim" }, -- For utility functions
     },
     config = function()
         local lint = require("lint")
+        -- local util = require("conform.util")
+        -- -- Find eslint in monorepo
+        -- local eslint = lint.linters.eslint
+        -- eslint.cmd = function()
+        --     -- PLANNED: Replace with "where_is_my_executable" to relocate per file
+        --     --
+        --     local name = "eslint"
+        --     local local_binary = vim.fn.fnamemodify("./node_modules/.bin/" .. name, ":p")
+        --     return vim.loop.fs_stat(local_binary) and local_binary or name
+        --     --
+        --     -- local repo_dir = util.root_file({ "package-lock.json" })(config, ctx) or ""
+        --     -- local paths = { repo_dir .. "node_modules/.bin/eslint" }
+        --     -- return util.find_executable(paths, "eslint")(config, ctx)
+        -- end
 
+        -- All available linters: https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#available-linters
         lint.linters_by_ft = {
-            -- go = { "golangcilint" }, -- PLANNED: look at customization: https://github.com/mfussenegger/nvim-lint/issues/532#issue-2126623239
-            javascript = { "eslint_d" },
-            javascriptreact = { "eslint_d" },
+            -- go = { "golangcilint" },
+            javascript = { "eslint" },
+            javascriptreact = { "eslint" },
             lua = { "selene" },
-            nix = { "nix" },
-            protobuf = { "buf", "protolint" },
-            python = { "ruff" }, -- PLANNED: configure flake8/mypy/etc. based on existence in .venv
-            -- python = { "mypy", "flake8", "pylint", "ruff" },
+            -- nix = { "nix" },
+            -- protobuf = { "buf", "protolint" },
+            python = { "ruff" }, -- PLANNED: configure flake8/mypy/etc. (based on existence in .venv)
+            -- python = { "mypy", "flake8", "pylint", "ruff" }, -- FYI: override pylint colors to be OFF!
             sh = { "shellcheck" },
             sql = { "sqlfluff" },
             terraform = { "tflint" },
-            typescript = { "eslint_d" },
-            typescriptreact = { "eslint_d" },
+            typescript = { "eslint" },
+            typescriptreact = { "eslint" },
             yaml = { "yamllint" },
             zsh = { "zsh" },
-            -- PLANNED: configure additional linters: https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#available-linters
+            -- PLANNED: configure additional linters:
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
+            -- TODO: Recreate 'lint' based on current file in buffer
             callback = function() lint.try_lint() end,
         })
     end,
