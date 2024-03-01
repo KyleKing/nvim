5de08f41edf23352e4d210cc259f96cd1ac1c762
Kyle King <KyleKing@users.noreply.github.com>
2024-02-29 21:31:35 -0500

    refactor: merge mini-move into single mini install

--- lua/kyleking/plugins/mini/init.lua
+++ lua/kyleking/plugins/mini/init.lua
@@ -1,20 +1,23 @@
 -- Defaults are Alt (Meta) + hjkl. Works in both Visual and Normal modes
 -- Alt: https://github.com/hinell/move.nvim
-return {
-    "echasnovski/mini.move",
-    opts = {
+local function mini_move()
+    require("mini.move").setup({
         mappings = {
             -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
             left = "<leader>mh",
             right = "<leader>ml",
             down = "<leader>mj",
             up = "<leader>mk",
-
             -- Move current line in Normal mode
             line_left = "<leader>mh",
             line_right = "<leader>ml",
             line_down = "<leader>mj",
             line_up = "<leader>mk",
         },
-    },
+    })
+end
+
+return {
+    "echasnovski/mini.nvim",
+    config = function() mini_move() end,
 }
