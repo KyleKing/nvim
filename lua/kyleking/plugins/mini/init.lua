c70e86f4e194365f1e420f3c4f10ee8d97b1c0d1
Kyle King <KyleKing@users.noreply.github.com>
2024-02-29 21:41:40 -0500

    refactor: merge mini.brakceted into single mini

--- lua/kyleking/plugins/mini/init.lua
+++ lua/kyleking/plugins/mini/init.lua
@@ -1,19 +1,13 @@
--- Defaults are Alt (Meta) + hjkl. Works in both Visual and Normal modes
--- Alt: https://github.com/hinell/move.nvim
-local function mini_move()
-    require("mini.move").setup({
-        mappings = {
-            -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-            left = "<leader>mh",
-            right = "<leader>ml",
-            down = "<leader>mj",
-            up = "<leader>mk",
-            -- Move current line in Normal mode
-            line_left = "<leader>mh",
-            line_right = "<leader>ml",
-            line_down = "<leader>mj",
-            line_up = "<leader>mk",
-        },
+-- Use gc for toggling comments. Examples:
+--  gcip or gcc (line)
+--  or dgc (delete commented section using gc text-object)
+-- Additional configuration for nvim-ts-context-commentstring from  : https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#minicomment
+-- And see: https://github.com/echasnovski/mini.comment/blob/67f00d3ebbeae15e84584d971d0c32aad4f4f3a4/doc/mini-comment.txt#L87-L101
+local function mini_comment()
+    require("mini.comment").setup({
+        custom_commentstring = function()
+            return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+        end,
     })
 end
 
@@ -36,26 +30,43 @@ local function mini_files()
     })
 end
 
--- Use gc for toggling comments. Examples:
---  gcip or gcc (line)
---  or dgc (delete commented section using gc text-object)
--- Additional configuration for nvim-ts-context-commentstring from  : https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#minicomment
--- And see: https://github.com/echasnovski/mini.comment/blob/67f00d3ebbeae15e84584d971d0c32aad4f4f3a4/doc/mini-comment.txt#L87-L101
-local function mini_comment()
-    require("mini.comment").setup({
-        custom_commentstring = function()
-            return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
-        end,
+-- Defaults are Alt (Meta) + hjkl. Works in both Visual and Normal modes
+-- Alt: https://github.com/hinell/move.nvim
+local function mini_move()
+    require("mini.move").setup({
+        mappings = {
+            -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+            left = "<leader>mh",
+            right = "<leader>ml",
+            down = "<leader>mj",
+            up = "<leader>mk",
+            -- Move current line in Normal mode
+            line_left = "<leader>mh",
+            line_right = "<leader>ml",
+            line_down = "<leader>mj",
+            line_up = "<leader>mk",
+        },
     })
 end
 
 return {
     "echasnovski/mini.nvim",
     dependencies = {
-        { "nvim-tree/nvim-web-devicons" }, -- Required for mini.files
         { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } }, -- For mini.comment
+        { "nvim-tree/nvim-web-devicons" }, -- Required for mini.files
     },
     keys = {
+        -- Bindings for mini.bracketed
+        { "[c", desc = "Jump to previous comment block" },
+        { "]c", desc = "Jump to next comment block" },
+        { "[x", desc = "Jump to previous conflict marker" },
+        { "]x", desc = "Jump to next conflict marker" },
+        { "[d", desc = "Jump to previous diagnostic" },
+        { "]d", desc = "Jump to next diagnostic" },
+        { "[q", desc = "Jump to previous Quickfix list entry" },
+        { "]q", desc = "Jump to next Quickfix list entry" },
+        { "[t", desc = "Jump to previous Treesitter node" },
+        { "]t", desc = "Jump to next Treesitter node" },
         -- Bindings for mini.files
         {
             "<leader>e",
@@ -74,8 +85,9 @@ return {
         },
     },
     config = function()
-        mini_move()
-        mini_files()
+        require("mini.bracketed").setup({})
         mini_comment()
+        mini_files()
+        mini_move()
     end,
 }
