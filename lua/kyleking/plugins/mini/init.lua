63bd0bc5c1f1cbf0eb42c908e51abc5e1b0ce701
Kyle King <KyleKing@users.noreply.github.com>
2024-02-29 21:35:27 -0500

    refactor: merge mini-files with single mini

--- lua/kyleking/plugins/mini/init.lua
+++ lua/kyleking/plugins/mini/init.lua
@@ -17,7 +17,49 @@ local function mini_move()
     })
 end
 
+-- Adapted from: https://github.com/mrjones2014/dotfiles/blob/9914556e4cb346de44d486df90a0410b463998e4/nvim/lua/my/configure/mini_files.lua
+local function mini_files()
+    require("mini.files").setup({
+        content = {
+            filter = function(entry)
+                return entry.name ~= ".DS_Store"
+                    and entry.name ~= ".git"
+                    and entry.name ~= ".venv"
+                    and entry.name ~= "node_modules"
+            end,
+        },
+        windows = {
+            -- Whether to show preview of file/directory under cursor
+            preview = true,
+            width_preview = 80,
+        },
+    })
+end
+
 return {
     "echasnovski/mini.nvim",
-    config = function() mini_move() end,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+        {
+            "<leader>e",
+            function()
+                local minifiles = require("mini.files")
+                if vim.bo.ft == "minifiles" then
+                    minifiles.close()
+                else
+                    local file = vim.api.nvim_buf_get_name(0)
+                    local file_exists = vim.fn.filereadable(file) ~= 0
+                    minifiles.open(file_exists and file or nil)
+                    minifiles.reveal_cwd()
+                end
+            end,
+            desc = "Explorer",
+        },
+    },
+    config = function()
+        mini_move()
+        mini_files()
+    end,
 }
