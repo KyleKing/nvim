e4f580b1e444dc8856a986cf1647f23fcb808768
Kyle King <kyleking@roaminsight.com>
2024-05-21 15:13:03 -0400

    fix: turn off keys conflicting with tab navigation

--- lua/kyleking/plugins/mini/init.lua
+++ lua/kyleking/plugins/mini/init.lua
@@ -66,17 +66,19 @@ return {
     },
     event = "UIEnter",
     keys = {
-        -- Bindings for mini.bracketed
-        { "[c", desc = "Jump to previous comment block" },
-        { "]c", desc = "Jump to next comment block" },
-        { "[x", desc = "Jump to previous conflict marker" },
-        { "]x", desc = "Jump to next conflict marker" },
-        { "[d", desc = "Jump to previous diagnostic" },
-        { "]d", desc = "Jump to next diagnostic" },
-        { "[q", desc = "Jump to previous Quickfix list entry" },
-        { "]q", desc = "Jump to next Quickfix list entry" },
-        { "[n", desc = "Jump to previous Treesitter node" },
-        { "]n", desc = "Jump to next Treesitter node" },
+        -- PLANNED: revisit bracketed bindings vs. existing TreeSitter bindings
+        -- -- Bindings for mini.bracketed
+        -- { "[c", desc = "Jump to previous comment block" },
+        -- { "]c", desc = "Jump to next comment block" },
+        -- { "[x", desc = "Jump to previous conflict marker" },
+        -- { "]x", desc = "Jump to next conflict marker" },
+        -- { "[d", desc = "Jump to previous diagnostic" },
+        -- { "]d", desc = "Jump to next diagnostic" },
+        -- { "[q", desc = "Jump to previous Quickfix list entry" },
+        -- { "]q", desc = "Jump to next Quickfix list entry" },
+        -- { "[n", desc = "Jump to previous Treesitter node" },
+        -- { "]n", desc = "Jump to next Treesitter node" },
+
         -- Bindings for mini.files
         {
             "<leader>e",
