e85d8c78339b2df5462bfe5dc38516bcb1b371e0
Kyle King <kyleking@roaminsight.com>
2024-05-30 12:38:44 -0400

    build: update trouble to v3

--- lua/kyleking/plugins/lsp/trouble.lua
+++ lua/kyleking/plugins/lsp/trouble.lua
@@ -1,15 +1,40 @@
 return {
     "folke/trouble.nvim",
-    cmd = { "Trouble", "TroubleToggle" },
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-        "folke/lsp-colors.nvim",
-    },
+    ---@class trouble.Config
     opts = {
         auto_close = true,
         use_diagnostic_signs = true,
     },
     keys = {
-        { "<leader>ut", "<cmd>TroubleToggle<cr>", desc = "Show Trouble" },
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
     },
 }
