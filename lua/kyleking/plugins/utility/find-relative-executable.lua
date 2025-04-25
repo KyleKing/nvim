128cc8dbb63426b5e958aa143a7507e47160d4a2
Kyle King <KyleKing@users.noreply.github.com>
2025-04-24 20:22:38 -0600

    Revert "hack: experimenting with custom plugins"
    This reverts commit c4f44aa26f1985383163dca3307152c4bd11d49a.

--- lua/kyleking/plugins/utility/find-relative-executable.lua
+++
@@ -1,7 +0,0 @@
----@class LazyPluginSpec
-return {
-    dir = "~/Developer/kyleking/find-relative-executable.nvim",
-    name = "find-relative-executable",
-    -- options = {}, -- PLANNED: This should be all that is needed
-    config = function() require("find-relative-executable").setup() end,
-}
