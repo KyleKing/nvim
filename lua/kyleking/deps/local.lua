e964d09cb8fe66ef2f5f3ab19f8b7e6315dccade
Kyle King <KyleKing@users.noreply.github.com>
2025-11-02 21:33:02 -0600

    fix: add local.lua placeholder

---
+++ lua/kyleking/deps/local.lua
@@ -0,0 +1,11 @@
+-- PLANNED: test new packages
+
+-- local MiniDeps = require("mini.deps")
+-- local add, now, _later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+-- now(function()
+--     add({
+--         source = "file:///Users/kyleking/Developer/local-code/spaghetti-comb.nvim",
+--     })
+--     require("spaghetti-comb").setup({})
+-- end)
