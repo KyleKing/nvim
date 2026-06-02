5de861563cffb01a78abedf43a6d4383cc53769e
Kyle King <KyleKing@users.noreply.github.com>
2026-06-01 21:12:52 -0600

    fix: replace deprecated vim.lsp.with

--- lua/kyleking/deps/lsp.lua
+++ lua/kyleking/deps/lsp.lua
@@ -3,7 +3,14 @@ local deps_utils = require("kyleking.deps_utils")
 local add, later = MiniDeps.add, deps_utils.maybe_later
 
 -- Configure signature help border globally
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+vim.lsp.handlers["textDocument/signatureHelp"] = function(err, result, ctx, config)
+    return vim.lsp.handlers.signature_help(
+        err,
+        result,
+        ctx,
+        vim.tbl_extend("force", config or {}, { border = "rounded" })
+    )
+end
 
 later(function()
     add("mfussenegger/nvim-lint")
